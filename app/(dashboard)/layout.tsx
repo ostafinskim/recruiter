bdd69c8680ba6ef9563493fe62f5add41c06523e
Miro Ostafiński <ostafinskim@gmail.com>
2024-11-09 21:11:22 +0000

    Update dashboard layout: adjust grid columns for sidebar and main content

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,11 +5,11 @@ import { PropsWithChildren } from "react";
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <>
-      <main className="grid lg:grid-cols-8">
+      <main className="grid lg:grid-cols-6">
         <div className="hidden lg:block lg:col-span-1 lg:min-h-screen">
           <Sidebar />
         </div>
-        <div className="lg:col-span-7">
+        <div className="lg:col-span-5">
           <Navbar />
           <div className="py-16 px-4 sm:px-8 lg:px-16">{children}</div>
         </div>
